74c59a588668ec77b8bd056907dbd49bb8065d51
ACTILAZION <chemkhien255@gmail.com>
2026-07-02 16:04:18 +0700

    chore: remove legacy layer-based packages

--- src/main/java/com/actilazion/aries_transaction/entity/enums/AccountStatus.java
+++
@@ -1,7 +0,0 @@
-package com.actilazion.aries_transaction.entity.enums;
-
-public enum AccountStatus {
-    ACTIVE,
-    FROZEN,
-    CLOSED
-}
--- src/main/java/com/actilazion/aries_transaction/entity/enums/OutboxEventStatus.java
+++
@@ -1,7 +0,0 @@
-package com.actilazion.aries_transaction.entity.enums;
-
-public enum OutboxEventStatus {
-    PENDING,
-    PUBLISHED,
-    FAILED
-}
